867bc81c50aead64eea0b59bbb6ced6f90229470
Beka <beksultantoksonbaev88@gmail.com>
2022-09-08 00:42:37 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String myname = "Beksultan";
+        System.out.println("Hello: " + myname);
+        String NUM = "Beksultan";
+        int x = 10;
+        System.out.println(x);
+        int vfioueofconstsnt = 10;
+        if (vfioueofconstsnt < 10){
+            System.out.println("Вы сохранили отритцательное число");
+        }
+
+        if (vfioueofconstsnt > 0){
+            System.out.println("Вы сохранили положительное число");
+        }
+
+        if (vfioueofconstsnt < 10){
+            System.out.println("Вы сохранили отритцательное число");
+        } else {
+            System.out.println("Вы сохранили ноль");
+        }
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
